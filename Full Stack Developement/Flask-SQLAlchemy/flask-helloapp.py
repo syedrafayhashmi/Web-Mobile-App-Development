f868d5ecdfc5246a537616ecb8a3fb8a112276a2
syedrafayhashmi <rafaysidsid@gmail.com>
2020-03-31 21:40:43 +0500

    fsd

--- Full Stack Developement/Flask-SQLAlchemy/flask-helloapp.py
+++ Full Stack Developement/Flask-SQLAlchemy/flask-helloapp.py
@@ -2,9 +2,19 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:12345678@127.0.0.1:5432/example'
+db  = SQLAlchemy(app) # database
+
+class Person(db.Model): 
+# don't need to define an init method because SQLAlchemy does it for you when you use it in a class
+    __tablename__ = 'persons'
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(), nullable = False)
+
+db.create_all()
 
 @app.route('/')
 def index():
     return "Hello World!"
 if __name__ == '__main__':
-    app.run()
+    app.run(debug = True)
