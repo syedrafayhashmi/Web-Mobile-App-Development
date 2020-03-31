ed086516efc89536dd44dd8a007795a56d9a3259
syedrafayhashmi <rafaysidsid@gmail.com>
2020-03-31 22:17:37 +0500

    fsd

--- Full-Stack-Developement/Flask-SQLAlchemy/flask_hello_app.py
+++ Full-Stack-Developement/Flask-SQLAlchemy/flask_hello_app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:12345678@127.0.0.1:5432/example'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #for warning
 db  = SQLAlchemy(app) # database
 
 class Person(db.Model): 
@@ -15,6 +16,7 @@ db.create_all()
 
 @app.route('/')
 def index():
-    return "Hello World!"
+    person = Person.query.first()
+    return "Hello " + person.name + " your ID is: " + str(person.id) 
 if __name__ == '__main__':
     app.run(debug = True)
