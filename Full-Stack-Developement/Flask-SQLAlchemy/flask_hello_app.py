452513a771cc62e4bb61072bbb67c083ba0b194f
syedrafayhashmi <rafaysidsid@gmail.com>
2020-03-31 23:38:54 +0500

    fsd

--- Full-Stack-Developement/Flask-SQLAlchemy/flask_hello_app.py
+++ Full-Stack-Developement/Flask-SQLAlchemy/flask_hello_app.py
@@ -11,6 +11,8 @@ class Person(db.Model):
     __tablename__ = 'persons'
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(), nullable = False)
+    def __repr__(self):
+        return f'<Person ID : {self.id} , name: {self.name}>'
 
 db.create_all()
 
