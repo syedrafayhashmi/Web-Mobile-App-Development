6affcffa3744666b99d22344a483b207b9c007c1
syedrafayhashmi <rafaysidsid@gmail.com>
2020-04-02 00:12:32 +0500

    fsd

--- Full-Stack-Developement/todoapp/app.py
+++ Full-Stack-Developement/todoapp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -16,6 +16,13 @@ class Todo(db.Model):
         return f"<Todo {self.id} {self.description}>"
 
 db.create_all()
+@app.route('/todos/create', methods = ['POST'])
+def create_todo():
+    description = request.form.get('description','') # an empty string if nothing comes in
+    todo  = Todo(description=description)
+    db.session.add(todo)
+    db.session.commit()
+    return redirect(url_for('index')) # our index route
 @app.route('/')
 def index():
     return render_template('index.html', data = Todo.query.all())
