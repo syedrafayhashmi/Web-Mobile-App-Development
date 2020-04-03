c3b5a5d2c072b5e4a9d2693f5bce7c2b843911d9
syedrafayhashmi <rafaysidsid@gmail.com>
2020-04-04 01:00:25 +0500

    added delete CRUD

--- Full-Stack-Developement/todoapp_fetch/app.py
+++ Full-Stack-Developement/todoapp_fetch/app.py
@@ -3,7 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 import sys
 from flask_migrate import Migrate
 
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:12345678@127.0.0.1:5432/todoapp'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #for warning
