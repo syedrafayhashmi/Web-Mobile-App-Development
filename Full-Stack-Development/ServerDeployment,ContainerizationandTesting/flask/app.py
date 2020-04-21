9e7956dc0f94656e981926628d7d4dfbba3f55f3
syedrafayhashmi <rafaysidsid@gmail.com>
2020-04-21 20:59:41 +0500

    kubernetes

---
+++ Full-Stack-Development/ServerDeployment,ContainerizationandTesting/flask/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+APP = Flask(__name__)
+
+
+@APP.route('/')
+def hello_world():
+    return 'Hello, World from Flask!\n'
+
+
+
+if __name__ == '__main__':
+    APP.run(host='127.0.0.1', port=8080, debug=True)
