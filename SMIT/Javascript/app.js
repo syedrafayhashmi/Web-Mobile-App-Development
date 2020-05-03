69d9e5527b80f5fefcb86702fb706d8a2116ffb5
syedrafayhashmi <rafaysidsid@gmail.com>
2020-05-04 03:47:42 +0500

    operators

---
+++ SMIT/Javascript/app.js
@@ -0,0 +1,27 @@
+alert("Hello World!");
+var a = 3;
+var b = 7;
+var c = a+b;
+alert(c);
+
+console.log(a);
+var a = 5;
+var b = "5";
+var c = a+b;
+var d = a*b;
+var e = a/b;
+var f = a-b
+var g = a%b
+alert(c);
+alert(d);
+alert(e);
+alert(f);
+alert(g);
+var h = a**b;
+alert(h);
+var a =6;
+a++;
+alert(a);
+var x = 10;
+var y = x++ + ++x -x + x++ -++x + x;
+alert(y);
