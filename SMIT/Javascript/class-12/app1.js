8dff9e8dc35c1602a307958f00931fbd06d78851
syedrafayhashmi <rafaysidsid@gmail.com>
2020-06-04 23:53:22 +0500

    class-12

---
+++ SMIT/Javascript/class-12/app1.js
@@ -0,0 +1,8 @@
+// table of 2
+//for (var i = 1;i <=10 ; i++){document.write("2"+"x"+i +"=" + 2*i+ "<br>")} //table code
+
+// loop an array
+var nam = ["ali","rafay","zain","asad"];
+for (var j = 0 ; j<4; j++){
+    document.write("name is " +nam[j]+"<br>");
+}
