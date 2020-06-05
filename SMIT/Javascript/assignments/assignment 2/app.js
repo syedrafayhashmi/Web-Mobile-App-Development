fd5c83e1946418a0dfd3515d27f328fe358f6912
syedrafayhashmi <rafaysidsid@gmail.com>
2020-06-05 16:05:17 +0500

    assignment 2 JS

---
+++ SMIT/Javascript/assignments/assignment 2/app.js
@@ -0,0 +1,27 @@
+//Task 1 
+var username;
+
+//Task 2
+var myName = "Syed Rafay Hashmi";
+
+//Task 3
+var message = "Hello World";
+alert(message);
+
+//Task 4
+var name = prompt("Enter Your Name", "Your Name Here");
+var age = prompt("Enter Your Age", "Your Age Here");
+var course = prompt("Enter Your Course Name", "Your Course Name Here");
+alert(name);
+alert(age +" years old");
+alert(course)
+
+//Task 5
+var word = prompt("Enter any Word");
+var text = "";
+for (i =  word.length ; i>0; i--){
+    text += word.slice(0,i) + "\n"
+}
+alert(text)
+
+// Task 6
