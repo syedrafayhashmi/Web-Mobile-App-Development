42ce64b1a073f8cf3c6d704546917453574266e2
syedrafayhashmi <rafaysidsid@gmail.com>
2020-06-05 21:58:51 +0500

    assignment 5 JS

---
+++ SMIT/Javascript/assignments/assignment 5/app.js
@@ -0,0 +1,27 @@
+//Task 1
+var num1 = +prompt("enter number 1");
+var num2 = +prompt("enter number 2");
+var sum = num1+num2;
+alert("Sum of "+ num1 +" and " + num2 + " is " + sum);
+
+//Task 2
+var num1 = +prompt("enter number 1");
+var num2 = +prompt("enter number 2");
+var diff = num1-num2;
+alert("difference of "+ num1 +" and " + num2 + " is " + diff);
+
+var num1 = +prompt("enter number 1");
+var num2 = +prompt("enter number 2");
+var mult = num1*num2;
+alert("multiplication of "+ num1 +" and " + num2 + " is " + mult);
+
+var num1 = +prompt("enter number 1");
+var num2 = +prompt("enter number 2");
+var div = num1/num2;
+alert("division of "+ num1 +" and " + num2 + " is " + div);
+
+//Task 3
+var myVariable;
+document.write("Value after variable declaration is: "+ myVariable);
+var myVariable2 = 5;
+document.write("Initial value: "+ myVariable2);
