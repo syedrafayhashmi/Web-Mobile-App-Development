88ccbfb4ab346559b09cae2d46ae5ea6b304c096
syedrafayhashmi <rafaysidsid@gmail.com>
2020-06-20 23:58:52 +0500

    assignment 35-38

---
+++ SMIT/Javascript/Assignment 6/Chapter35-38/app.js
@@ -0,0 +1,13 @@
+// Task 1
+function dateTime(){
+    time = new Date();
+    return document.write(time+"<br>");
+}
+//dateTime();
+
+// Task 2
+function greet(){
+   var first = prompt("Enter First Name");
+   var last = prompt("Enter Last Name");
+
+}
