61a03a239a68c3a072e724ccce5dbf5901bc16aa
syedrafayhashmi <rafaysidsid@gmail.com>
2020-06-20 13:30:49 +0500

    assignment 21-25

--- SMIT/Javascript/Assignment 6/Chapters21-25/app.js
+++ SMIT/Javascript/Assignment 6/Chapters21-25/app.js
@@ -1,1 +1,23 @@
-alert("Hello")
+// Task 1
+var firstName = prompt("Write your First Name");
+var lastName = prompt("Write your Last Name");
+var fullName = firstName + " " + lastName;
+alert("Hello " + fullName);
+
+// Task 2
+var phone = prompt("Enter your Favorite Mobile Phone Model");
+document.write("My favorite phone is: "+ phone+"<br>");
+var len = phone.length;
+document.write("Length of string: "+ len+"<br>");
+
+// Task 3
+var str = "Pakistani";
+document.write("String: "+ str +"<br>");
+var ind = str.indexOf('n')
+document.write("Index of 'n': "+ ind+"<br>");
+
+// Task 4
+var str2 = "Hello World";
+document.write("String: "+ str2 +"<br>");
+var ind = str2.lastIndexOf('l')
+document.write("Last Index of 'l': "+ ind+"<br>");
