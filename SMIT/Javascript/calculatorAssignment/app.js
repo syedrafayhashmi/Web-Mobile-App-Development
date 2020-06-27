0dddbd2200d17a16b6f8497d2f088c32996055a1
syedrafayhashmi <rafaysidsid@gmail.com>
2020-06-27 15:50:01 +0500

    functions

--- SMIT/Javascript/calculatorAssignment/app.js
+++ SMIT/Javascript/calculatorAssignment/app.js
@@ -1,3 +1,12 @@
 function getNumber(num){
-    var result  = document.getElementById("")
+    var result  = document.getElementById("result");
+    result.value += num;
+}
+function clearResult(){
+    var result  = document.getElementById("result");
+    result.value = "";
+}
+function result(){
+    var result  = document.getElementById("result");
+    result.value = eval(result.value)
 }
