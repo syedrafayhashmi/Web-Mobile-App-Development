4deaf7a8660ce97daf80594ce2f5af97a161c37d
syedrafayhashmi <rafaysidsid@gmail.com>
2020-07-08 22:02:23 +0500

    read more function

---
+++ SMIT/Javascript/class-16/app.js
@@ -0,0 +1,5 @@
+function readMore(){
+    var text = "Lorem ipsum dolor sit amet, consectetur adipisicing Non labore distinctio reprehenderit, nobis nesciunt velit,magnam repellat ad soluta maiores minima eligendi fugit voluptate ut ipsam. Cum expedita alias eos!";
+    var paragraph  = document.getElementById("para");
+    paragraph.innerHTML = text;
+}
