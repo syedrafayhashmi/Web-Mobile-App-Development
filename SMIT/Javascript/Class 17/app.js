7287ca035fbf7dc84c3af988ab5a28ebb4af0c85
syedrafayhashmi <rafaysidsid@gmail.com>
2020-07-09 12:35:08 +0500

    stopwatch

--- SMIT/Javascript/Class 17/app.js
+++ SMIT/Javascript/Class 17/app.js
@@ -3,12 +3,12 @@ var count = 0;
 var interval;
 function timer(){
     count++;
-    console.log(count);
+    console.log(count); //timer function
 }
 
 function stopInterval(){
     clearInterval(interval);
-    console.log("Function stopped by another function");
+    console.log("Function stopped by another function"); // stops the timer function saved in interval variable
 }
-interval = setInterval(timer,1000);
-setTimeout(stopInterval,5000);
+interval = setInterval(timer,1000); // runs every one second
+setTimeout(stopInterval,5000); // stops after 5 seconds
