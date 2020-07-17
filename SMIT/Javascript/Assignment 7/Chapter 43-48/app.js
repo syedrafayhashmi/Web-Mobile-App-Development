ccbc94811edbf1f1a15134b68e2d1a317b3468af
syedrafayhashmi <rafaysidsid@gmail.com>
2020-07-17 23:37:39 +0500

    chap 43-48 done

---
+++ SMIT/Javascript/Assignment 7/Chapter 43-48/app.js
@@ -0,0 +1,26 @@
+// Task 1 See HTML
+
+// Task 2 See HTML
+
+// Task 3 Also see HTML
+function rowDelete(rowIndex){
+    document.getElementById("table").deleteRow(rowIndex);
+}
+
+// Task 4 Also See HTML
+function image1(){
+    var image = document.getElementById('img');
+    image.src = "1.jpg";
+}
+function image2(){
+    var image = document.getElementById('img');
+    image.src = "2.jpg";
+}
+// Task 5 Also See HTML
+var value = document.getElementById("count");
+function increase(){
+    value.innerHTML = +value.innerHTML + 1;
+}
+function decrease(){
+    value.innerHTML -=1;
+}
