284cbfa1ba811ff1088f4871d00a65f213aec799
syedrafayhashmi <rafaysidsid@gmail.com>
2020-07-24 23:19:44 +0500

    UI and functions

--- SMIT/Javascript/To-do App/app.js
+++ SMIT/Javascript/To-do App/app.js
@@ -4,15 +4,37 @@ var list = document.getElementById("item");
 function addTodo(){
     var todoValue = document.getElementById("todo-value");
     var li = document.createElement('li');
+    var div = document.createElement('div');
     var deleteButton = document.createElement('button');
-    deleteButton.className  = "btn text-white btn-danger";
-    var delText = document.createTextNode("Delete");
-    deleteButton.appendChild(delText)
-    li.className  = "list-group-item list-group-item-action list-group-item-success";
-    var liText = document.createTextNode(todoValue.value);
-    li.appendChild(liText);
-    list.appendChild(li);
-    list.appendChild(deleteButton);
+    var editButton = document.createElement('button');
 
+    div.style.display = "inline";
+    div.className = "list-group";
+    li.className  = "list-group list-group-item list-group-item-action list-group-item-success";
+    deleteButton.className  = "btn text-white btn-danger";
+    editButton.className  = "btn text-white btn-warning";
+
+    li.style.float = "left"; 
+    li.style.width = "40%";
+    deleteButton.style.width = "30%";
+    deleteButton.style.float = "left";
+    deleteButton.style.marginTop  = "5px";
+    editButton.style.width = "30%";
+    editButton.style.float = "left";
+    editButton.style.marginTop  = "5px";
+
+    var delText = document.createTextNode("Delete");
+    var editText = document.createTextNode("Edit");
+    var liText = document.createTextNode(todoValue.value);
+
+    editButton.appendChild(editText);
+    deleteButton.appendChild(delText);
+    li.appendChild(liText);
+    div.appendChild(li);
+    div.appendChild(editButton);
+    div.appendChild(deleteButton);
+    
+    list.appendChild(div);
+        
     todoValue.value = "";
 }
