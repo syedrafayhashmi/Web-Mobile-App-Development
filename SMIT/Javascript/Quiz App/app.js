80c78fece0d47ab865660a6e5cf62c2236558ec9
syedrafayhashmi <rafaysidsid@gmail.com>
2020-07-25 20:54:00 +0500

    questions added

---
+++ SMIT/Javascript/Quiz App/app.js
@@ -0,0 +1,58 @@
+var Questions = [
+    {
+        question : "What does HTML stand for?",
+        answers : {
+            a : "Hyper type marked language",
+            b : "Hyper text markup language",
+            c : "Hyper text makeup language",
+            d : "Hyper terrain mock language"
+        },
+        correctAnswer : "b"
+    },
+    {
+        question : "What does CSS stand for?",
+        answers : {
+            a : "Cascading Style Sheet",
+            b : "Complex Style Syntax",
+            c : "Complete Sound Sheet",
+            d : "Complete Sheet Style"
+        },
+        correctAnswer : "a"
+    },
+    {
+        question : "Inside which HTML element do we put the JavaScript?",
+        answers : {
+            a : "<js>",
+            b : "<javascript>",
+            c : "<scripting>",
+            d : "<script>"
+        },
+        correctAnswer : "d"
+    },
+    {
+        question : "How do you create a function in JavaScript?",
+        answers : {
+            a : "function:myFunction()",
+            b : "function = myFunction()",
+            c : "function myFunction()",
+            d : "function myFunction{}"
+        },
+        correctAnswer : "c"
+    },
+    {
+        question : "How can you add a comment in a JavaScript?",
+        answers : {
+            a : "//Comment",
+            b : "/*Comment*/",
+            c : "<!--Comment-->",
+            d : "#Comment"
+        },
+        correctAnswer : "a"
+    },
+    
+]
+
+var list = document.getElementById("item");
+
+
+
