8590c9296929076aa44f91084f91c04bdb9ee6b5
syedrafayhashmi <rafaysidsid@gmail.com>
2020-07-25 14:36:42 +0500

    update button done

--- SMIT/Javascript/To-do App/app.js
+++ SMIT/Javascript/To-do App/app.js
@@ -1,8 +1,8 @@
 var list = document.getElementById("item");
-var div = document.createElement('div');
 
 
 function addTodo(){
+    var div = document.createElement('div');
     var todoValue = document.getElementById("todo-value");
     var disIp = document.createElement('input');
     var deleteButton = document.createElement('button');
@@ -38,11 +38,24 @@ function addTodo(){
 
     disIp.value = todoValue.value;
 
+    
+    var updateButton = document.createElement('button');
+    updateButton.className = "btn text-white btn-warning";
+    updateButton.setAttribute("onclick","updateItem(this)");
+    updateButton.style.width = "30%";
+    updateButton.style.float = "left"; 
+    var updateText = document.createTextNode("Update");
+    updateButton.appendChild(updateText);
+    updateButton.style.display = "none";
+    
+    
     editButton.appendChild(editText);
     deleteButton.appendChild(delText);
     div.appendChild(disIp);
     div.appendChild(editButton);
+    div.appendChild(updateButton)
     div.appendChild(deleteButton);
+    
     
     if(todoValue.value !== ""){
         list.appendChild(div);
@@ -59,19 +72,20 @@ function deleteAll(){
     list.innerHTML = ""
 }
 function editItem(e){
-    var updateButton = document.createElement('button');
-    updateButton.className = "btn text-white btn-warning";
-    updateButton.setAttribute("onclick","updateItem(this)");
-    updateButton.style.width = "30%";
-    updateButton.style.float = "left"; 
-    var updateText = document.createTextNode("Update");
-    updateButton.appendChild(updateText);
-
+    console.log(e.sibling)
 
     e.parentNode.firstChild.removeAttribute("disabled");
-    e.parentNode.firstChild.focus();
+    e.parentNode.firstChild.focus();                       
     e.parentNode.children[1].style.display = "none";
-    div.insertBefore(updateButton,e.parentNode.children[1]);
+    e.parentNode.children[2].style.display = "";
+    
+
+}
+
+function updateItem(e){
+    e.parentNode.firstChild.setAttribute("disabled","");
+    e.parentNode.children[1].style.display = ""; 
+    e.parentNode.children[2].style.display = "none"; 
 
 }
 
