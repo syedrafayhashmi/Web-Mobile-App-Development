988f07b6411fa44ac5670688c11b1a5ecc0c127b
syedrafayhashmi <rafaysidsid@gmail.com>
2020-08-15 14:52:56 +0500

    firebase

--- SMIT/Firebase DB/Firebase/public/app.js
+++ SMIT/Firebase DB/Firebase/public/app.js
@@ -12,6 +12,19 @@ function pushData(){
  //or firebase.database().ref('student/student1').set(student)
  //firebase.database().ref('student').push(student)
 
-  firebase.database().ref('student').push(student)
+  firebase.database().ref('student/'+key).set(student)
 
 }
+
+function getFirebaseData(){
+    firebase.database().ref('student').once('value', function(data){
+        console.log(data.val())
+    })
+}
+//getFirebaseData()
+function getFirebaseDataRealTime(){
+    firebase.database().ref('student').on('child_added', function(data){
+        console.log(data.val())
+    })
+}
+//getFirebaseDataRealTime()
