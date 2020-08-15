62af82df4f753e66f50e0b2f7119910b2c7bd947
syedrafayhashmi <rafaysidsid@gmail.com>
2020-08-15 13:45:47 +0500

    firebase

--- SMIT/Firebase DB/Firebase/app.js
+++ SMIT/Firebase DB/Firebase/app.js
@@ -1,9 +1,17 @@
 function pushData(){
     var name  = document.getElementById("name");
     var roll  = document.getElementById("roll");
+    var key = firebase.database().ref('student').push().key
     var student = {
         name : name.value,
-        roll : roll.value
+        roll : roll.value,
+        key : key
     }
-    console.log(student)    
+ //firebase.database().ref('student').set(student)
+ //firebase.database().ref('student').child('student1').set(student) 
+ //or firebase.database().ref('student/student1').set(student)
+ //firebase.database().ref('student').push(student)
+
+  firebase.database().ref('student').push(student)
+
 }
