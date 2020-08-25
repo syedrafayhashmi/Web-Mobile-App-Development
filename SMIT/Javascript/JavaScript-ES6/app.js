6c8ecb510002490a04668a2b304c2f476edb48e7
syedrafayhashmi <rafaysidsid@gmail.com>
2020-08-26 02:53:32 +0500

    es6

--- SMIT/Javascript/JavaScript-ES6/app.js
+++ SMIT/Javascript/JavaScript-ES6/app.js
@@ -2,3 +2,11 @@ var x = "myname";
 var x = "yourname";
 let a = "a";
 const name = "RAFAY";
+
+let firstname = "SYED RAFAY";
+let lastname = "HASHMI"
+console.log(`My name is ${firstname} ${lastname}`);
+
+var age = 31;
+var check = age>30? "greater than 30": "less than 30";
+console.log(check);
