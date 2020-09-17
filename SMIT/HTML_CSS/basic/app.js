7e039ccb95af4d7516c422daa72e08d3679bcee2
syedrafayhashmi <rafaysidsid@gmail.com>
2020-09-18 02:55:26 +0500

    setting meta

---
+++ SMIT/HTML_CSS/basic/app.js
@@ -0,0 +1,5 @@
+let head = document.getElementsByTagName('head')[0];
+let meta = document.createElement('meta');
+meta.setAttribute("name","description");
+meta.setAttribute("content","ELECTRONIC ENGINEERING STUDENT | FULL STACK DEVELOPER | AI & DATA SCIENCE PRACTITIONER");
+head.insertAdjacentElement('afterbegin',meta);
