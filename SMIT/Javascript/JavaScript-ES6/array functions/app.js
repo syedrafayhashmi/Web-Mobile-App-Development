f364704d7f4ded4957384643cd94a0cf741b0778
syedrafayhashmi <rafaysidsid@gmail.com>
2020-10-07 03:21:02 +0500

    search

--- SMIT/Javascript/JavaScript-ES6/array functions/app.js
+++ SMIT/Javascript/JavaScript-ES6/array functions/app.js
@@ -1,4 +1,15 @@
+// FILTER
+
 let arr = [{name: "rafay", age : 21}, {name: "ali", age : 16}];
 arr.filter((a)=> console.log(a)); //filtering an array
 let filter = arr.filter((a)=> console.log(a));
 
+
+// SEARCH
+
+let name = "rafay";
+let search = "r";
+
+if(name.startsWith(search)){
+    console.log(name);
+}
