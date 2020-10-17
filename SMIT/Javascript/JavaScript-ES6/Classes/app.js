255a620d7309e938c8fcb8987e0737b25a5c432d
syedrafayhashmi <rafaysidsid@gmail.com>
2020-10-18 01:55:32 +0500

    es6 classes

--- SMIT/Javascript/JavaScript-ES6/Classes/app.js
+++ SMIT/Javascript/JavaScript-ES6/Classes/app.js
@@ -5,3 +5,12 @@ class Student {
     }
 }
 console.log(new Student("Syed","syed@gmail.com"));
+
+class School extends Student{
+    constructor(n,e,school){
+        super(n,e)
+            this.school = school;
+
+    }
+}
+console.log(new School("Syed","syed@gmail.com","world"));
