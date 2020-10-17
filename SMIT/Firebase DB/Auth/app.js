2ba5617587b1f59fbca73d985fc9c0f6ebd60a5c
syedrafayhashmi <rafaysidsid@gmail.com>
2020-10-18 03:02:46 +0500

    auth

--- SMIT/Firebase DB/Auth/app.js
+++ SMIT/Firebase DB/Auth/app.js
@@ -13,3 +13,22 @@ let signup = () =>{
         console.log(errorMessage);
       });email-password.html
 }
+
+let login = () =>{
+    let email = document.getElementById("login-email");
+    let password = document.getElementById("login-password");
+    firebase.auth().signInWithEmailAndPassword(email, password)
+    .then((result)=>{
+        console.log(Logged In);
+        console.log(result)
+    }
+    ).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ...
+        console.log(errorMessage);
+      });email-password.html
+
+}
+
