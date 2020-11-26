af3121f7958a7132e54a0cb7f34f2d09a8d8ac09
syedrafayhashmi <rafaysidsid@gmail.com>
2020-11-26 15:25:47 +0500

    react udemy comp

--- React-Udemy/react-complete-guide/src/App.js
+++ React-Udemy/react-complete-guide/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import Person from './Person/Person'
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hello, from React App</h1>
+        <Person/>
       </div>
     );
     // return (
---
+++ React-Udemy/react-complete-guide/src/Person/Person.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Person  = () => {
+    return(
+        <p>I'm  a Person!</p>
+    );
+}
+export default Person;
