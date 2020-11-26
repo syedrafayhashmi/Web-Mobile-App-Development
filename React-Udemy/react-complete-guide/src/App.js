9313a0e598ead89693d97ff22fda8b1a43cdc97a
syedrafayhashmi <rafaysidsid@gmail.com>
2020-11-26 22:50:31 +0500

    react udemy event hooks bf

--- React-Udemy/react-complete-guide/src/App.js
+++ React-Udemy/react-complete-guide/src/App.js
@@ -7,20 +7,28 @@ class App extends Component {
       {name:"Rafay", age : 21},
       {name:"Ali", age : 16},
       {name:"Hashmi", age : 25},
-    ]
-  }
+    ],
+    otherState : "something"
+  };
 switchNameHandler = () =>{
-  
+  this.setState({
+    persons: [
+      {name:" Syed Rafay", age : 21},
+      {name:"Ali", age : 16},
+      {name:"Hashmi", age : 24},
+    ]
+  })
 }
+
   render() {
     return (
       <div className="App">
-        <button onClick = {this.switchNameHandler} >Switch Name</button>
-        
+               
         <h1>Hello, from React App</h1>
+        <button onClick = {this.switchNameHandler} >Switch Name</button>
         <Person name = {this.state.persons[0].name} age = {this.state.persons[0].age}>I am elder</Person>
         <Person name = {this.state.persons[1].name} age = {this.state.persons[1].age}>I am younger</Person>
-        <Person name = {this.state.persons[2].name} age = {this.state.persons[2].name}>I am eldest</Person>
+        <Person name = {this.state.persons[2].name} age = {this.state.persons[2].age}>I am eldest</Person>
       </div>
     );
     // return (
